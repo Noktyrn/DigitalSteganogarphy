9b6b27b4a75b53193981e87aff492d949b6f7633
Tagir Rakipov <trrakipov@miem.hse.ru>
2021-08-17 20:03:08 +0500

    added iterative embedding for AddEmb method

--- Algorithms/AddEmb.py
+++ Algorithms/AddEmb.py
@@ -1,5 +1,5 @@
 import numpy as np
-from Utils.utils import cut_image_into_blocks, get_dct_coefs, quantize_dct_blocks, qm, concatenate_image, get_image_from_dct_coefs, dequantize_dct_blocks
+from Utils.utils import count_BER, cut_image_into_blocks, get_dct_coefs, quantize_dct_blocks, qm, concatenate_image, get_image_from_dct_coefs, dequantize_dct_blocks
 
 def cost(u, v):
     """
@@ -71,11 +71,6 @@ def get_message_from_block(mes, positions, b, emb_l):
         elif I == -1 or I == 2:
             emb_l -= 1
             mes.append(1)
-        
-        if I >= 2:
-            b[p[0]][p[1]] -= 1
-        elif I <= -1:
-            b[p[0]][p[1]] += 1
     
     return mes, b, emb_l
 
@@ -91,7 +86,7 @@ def get_capacity(positions, block_vector):
     return cap
     
 
-def insert_message_AddEmb(image, message, k=1):
+def insert_message_AddEmb(image, message, k=8):
     l = len(message)
 
     #step1
@@ -151,3 +146,68 @@ def extract_message_AddEmb(im, route, mes_len):
             break
     
     return extracted_message
+
+def insert_message_iAddEmb(image, message, k=1, threshold=10):
+    l = len(message)
+
+    #step1
+    blocks = cut_image_into_blocks(image)
+    dct_blocks = quantize_dct_blocks(get_dct_coefs(blocks))
+    
+    #step2
+    b_vector = dct_blocks.reshape(4096, 8, 8)
+    
+    n = len(b_vector[0])
+    num_blocks = len(dct_blocks)
+
+    #step3
+    Q = [[] for _ in range(n)]
+    for i in range(num_blocks):
+        u = i // n
+        v = i %  n
+        Q[u].append(dist_func(u, v, b_vector))
+    
+    #step4
+    k_coords = []
+    for i in range(n):
+        for j in range(n):
+            k_coords.append([i, j])
+    
+    k_mins = sorted(k_coords, key=lambda x: Q[x[0]][x[1]])
+
+    embedding_positions = k_mins[:k]
+    cap = get_capacity(embedding_positions, b_vector)
+
+    if cap >= l:
+        point = 0
+        for idx in range(len(b_vector)):
+            t = 0
+            cur_ber = 1
+            best_ber = 1
+            
+            temp_point = point
+            best_point = temp_point
+
+            temp_b = b_vector[idx]
+            best_b = temp_b
+
+            while t < threshold and cur_ber != 0:
+                temp_point, temp_b = embed_message_into_block(point, message, embedding_positions, b_vector[idx])
+                old_mes = message[point:temp_point]
+                new_mes, _, _ = get_message_from_block([], embedding_positions, b_vector[idx], temp_point-point)
+                cur_ber = count_BER(old_mes, new_mes)
+
+                if cur_ber < best_ber:
+                    best_ber = cur_ber
+                    best_b = temp_b
+                    best_point = temp_point
+            
+            point = best_point
+            b_vector[idx] = best_b
+            if point >= l:
+                break
+
+        new_image = concatenate_image(get_image_from_dct_coefs(dequantize_dct_blocks(b_vector.reshape(64, 64, 8, 8))))
+        return new_image, {'route': embedding_positions, 'mes_len':l}
+    else:
+        raise RuntimeError("Capacity {} can't fit the message with size {}".format(cap, l))
